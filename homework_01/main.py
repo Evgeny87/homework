2cb95dc4f45fdd7a9abc1720c324ebfd1dad3d27
evgeny <sun_nik_off@mail.ru>
2022-09-29 21:33:23 +0300

    Changed the file main

--- homework_01/main.py
+++ homework_01/main.py
@@ -4,13 +4,14 @@
 """
 
 
-def power_numbers():
+def power_numbers(*nums):
     """
     функция, которая принимает N целых чисел,
     и возвращает список квадратов этих чисел
     >>> power_numbers(1, 2, 5, 7)
     <<< [1, 4, 25, 49]
     """
+    return [i ** 2 for i in nums]
 
 
 # filter types
@@ -19,7 +20,7 @@ EVEN = "even"
 PRIME = "prime"
 
 
-def filter_numbers():
+def filter_numbers(numbers, filter_type):
     """
     функция, которая на вход принимает список из целых чисел,
     и возвращает только чётные/нечётные/простые числа
@@ -30,3 +31,32 @@ def filter_numbers():
     >>> filter_numbers([2, 3, 4, 5], EVEN)
     <<< [2, 4]
     """
+
+    def filter_even_num(in_num):
+        if (in_num % 2) == 0:
+            return True
+        else:
+            return False
+
+    def filter_odd_num(in_num):
+        if (in_num % 2) != 0:
+            return True
+        else:
+            return False
+
+    # кусок кода от сюда https://foxford.ru/wiki/informatika/proverka-chisla-na-prostotu-v-python
+    def filter_is_prime_num(in_num):
+        if in_num > 1:
+            if in_num % 2 == 0:
+                return in_num == 2
+            d = 3
+            while d * d <= in_num and in_num % d != 0:
+                d += 2
+            return d * d > in_num
+
+    if filter_type == ODD:
+        print(list(filter(filter_odd_num, numbers)))
+    elif filter_type == EVEN:
+        print(list(filter(filter_even_num, numbers)))
+    elif filter_type == PRIME:
+        print(list(filter(filter_is_prime_num, numbers)))
