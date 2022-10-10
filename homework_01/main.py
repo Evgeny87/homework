92218d7e1a43588ad223f678d6561f9a00b13402
evgeny <sun_nik_off@mail.ru>
2022-10-10 20:49:35 +0300

    Changed the file main

--- homework_01/main.py
+++ homework_01/main.py
@@ -19,7 +19,7 @@ EVEN = "even"
 PRIME = "prime"
 
 
-def filter_numbers():
+def filter_numbers(numbers_list, filter_type):
     """
     функция, которая на вход принимает список из целых чисел,
     и возвращает только чётные/нечётные/простые числа
@@ -30,3 +30,19 @@ def filter_numbers():
     >>> filter_numbers([2, 3, 4, 5], EVEN)
     <<< [2, 4]
     """
+    # кусок кода от сюда https://foxford.ru/wiki/informatika/proverka-chisla-na-prostotu-v-python
+    def filter_is_prime_num(in_num):
+        if in_num > 1:
+            if in_num % 2 == 0:
+                return in_num == 2
+            d = 3
+            while d * d <= in_num and in_num % d != 0:
+                d += 2
+            return d * d > in_num
+
+    if filter_type == ODD:
+        return [number for number in numbers_list if number % 2 != 0]
+    if filter_type == EVEN:
+        return [number for number in numbers_list if number % 2 == 0]
+    if filter_type == PRIME:
+        return list(filter(filter_is_prime_num, numbers_list))
