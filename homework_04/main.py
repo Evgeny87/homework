12e96162e73231e5bdbeeb75ff5d23d578ebf5aa
evgeny <sun_nik_off@mail.ru>
2023-01-05 16:51:28 +0300

    finish homework_04

--- homework_04/main.py
+++ homework_04/main.py
@@ -13,13 +13,64 @@
 - закрытие соединения с БД
 """
 
+import asyncio
+import aiohttp
+
+from jsonplaceholder_requests import fetch_data, USERS_DATA_URL, POSTS_DATA_URL
+from models import async_engine, Base, User, Post, Session
+
+
+async def fetch_users_data():
+    async with aiohttp.ClientSession():
+        users_data = []
+        data = await fetch_data(USERS_DATA_URL)
+        for user in data:
+            users_data.append({x: user.get(x) for x in ("name", "username", "email")})
+    return users_data
+
+
+async def fetch_posts_data():
+    async with aiohttp.ClientSession():
+        posts_data = []
+        data = await fetch_data(POSTS_DATA_URL)
+        for post in data:
+            posts_data.append({x: post.get(x) for x in ("userId", "title", "body")})
+    return posts_data
+
+
+async def create_tables(conn):
+    await conn.run_sync(Base.metadata.drop_all)
+    await conn.run_sync(Base.metadata.create_all)
+
+
+async def create_users(session, user):
+    user = User(name=user.get("name"), username=user.get("username"), email=user.get("email"))
+    session.add(user)
+
+
+async def create_posts(session, post):
+    post = Post(user_id=post.get("userId"), title=post.get("title"), body=post.get("body"))
+    session.add(post)
+
 
 async def async_main():
-    pass
+    async with async_engine.begin() as conn:
+        await create_tables(conn)
+
+    async with Session() as session:
+        users_data, posts_data = await asyncio.gather(
+            fetch_users_data(),
+            fetch_posts_data()
+        )
+        for user in users_data:
+            await create_users(session, user)
+        for post in posts_data:
+            await create_posts(session, post)
+        await session.commit()
 
 
 def main():
-    pass
+    asyncio.run(async_main())
 
 
 if __name__ == "__main__":
--- homework_04/models.py
+++ homework_04/models.py
@@ -10,7 +10,45 @@
 
 import os
 
+from sqlalchemy import (
+    Column,
+    String,
+    Integer,
+    ForeignKey
+)
+
+from sqlalchemy.orm import sessionmaker, declarative_base, relationship
+from sqlalchemy.ext.asyncio import create_async_engine, AsyncEngine, AsyncSession
+
 PG_CONN_URI = os.environ.get("SQLALCHEMY_PG_CONN_URI") or "postgresql+asyncpg://postgres:password@localhost/postgres"
 
-Base = None
-Session = None
+async_engine: AsyncEngine = create_async_engine(PG_CONN_URI, echo=False)
+
+Base = declarative_base(bind=async_engine)
+
+Session = sessionmaker(
+    async_engine,
+    expire_on_commit=False,
+    class_=AsyncSession
+)
+
+class User(Base):
+    __tablename__ = 'users'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String, nullable=False)
+    username = Column(String(20), unique=True)
+    email = Column(String, unique=True)
+
+    posts = relationship('Post', back_populates='user')
+
+
+class Post(Base):
+    __tablename__ = 'posts'
+
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey('users.id'), nullable=False)
+    title = Column(String, nullable=False)
+    body = Column(String)
+
+    user = relationship('User', back_populates='posts')
