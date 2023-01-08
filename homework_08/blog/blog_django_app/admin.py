0621266d6495998f065e9d45d5ee0711dc45a353
evgeny <sun_nik_off@mail.ru>
2023-01-08 12:44:07 +0300

    copy homework_07

---
+++ homework_08/blog/blog_django_app/admin.py
@@ -0,0 +1,20 @@
+from django.contrib import admin
+
+# Register your models here.
+from .models import User, Post
+
+
+class UserAdmin(admin.ModelAdmin):
+    list_display = ("id", "name", "email")
+
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ("id", "title", "user")
+
+    def get_queryset(self, request):
+        queryset = super().get_queryset(request)
+        return queryset.select_related("user")
+
+
+admin.site.register(User, UserAdmin)
+admin.site.register(Post, PostAdmin)
